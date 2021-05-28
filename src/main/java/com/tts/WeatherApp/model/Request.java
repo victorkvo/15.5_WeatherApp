7476fa36ccbed95dd00ee9a1903dc218b661e057
Victor Vo <79146455+victorkvo@users.noreply.github.com>
2021-05-28 15:50:33 -0500

    fixed some code

---
+++ src/main/java/com/tts/WeatherApp/model/Request.java
@@ -0,0 +1,10 @@
+package com.tts.WeatherApp.model;
+
+import lombok.Data;
+
+@Data
+public class Request {
+
+    private String zipCode;
+
+}
--- src/main/java/com/tts/WeatherApp/model/Response.java
+++ src/main/java/com/tts/WeatherApp/model/Response.java
@@ -9,15 +9,14 @@ import java.util.Map;
 @Data
 @NoArgsConstructor
 public class Response {
-
-    private Map<String, String> coord;
-    private List<Map<String, String>> weather;
+    private Map<String,String> coord;
+    private List<Map<String,String>> weather;
     private String base;
-    private Map<String, String> main;
-    private Map<String, String> wind;
-    private Map<String, String> clouds;
+    private Map<String,String> main;
+    private Map<String,String> wind;
+    private Map<String,String> clouds;
     private String dt;
-    private Map<String, String> sys;
+    private Map<String,String> sys;
     private String id;
     private String name;
     private String cod;
